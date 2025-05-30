b94da891792725ccc65086b9b033b15ecd43e70f
Subham Lamichhane <lamichhanesubham4@gmail.com>
2025-05-30 06:05:42 -0400

    Add files via upload

---
+++ mailsender.py
@@ -0,0 +1,13 @@
+import smtplib
+sender="scarofcupid@gmail.com"
+receiver="sharmadevi2077@gmail.com"
+password=""
+message="Hello,This is an example of test mail!! "
+try :
+ with smtplib.SMTP('smtp.gmail.com',587) as server :
+     server.starttls()
+     server.login(sender,password)
+     server.sendmail(sender,receiver,message)
+     print("Message sent !!")
+except ConnectionRefusedError:
+    print("SMTP isn't installed by default and the connection is refused as a result !!")
